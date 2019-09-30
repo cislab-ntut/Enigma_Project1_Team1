2cc767dd6fc206fdf1f163117ca4758fb4dda88e
ss303055 <50259316+ss303055@users.noreply.github.com>
2019-10-01 00:02:35 +0800

    start destination
    randomly enter start destination.

---
+++ enigma_startdestination.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+char etw[26] = { 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+	for (int i = 0; i < 26; i++){
+		for (int j = 0; j < 26; j++) {
+			for (int k = 26; k < 26; k++) {
+				etw[i];
+				etw[j];
+				etw[k];
+			}
+
+		}
+
+}
+}	
